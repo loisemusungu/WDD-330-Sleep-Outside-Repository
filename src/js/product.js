f9a8d3ddb958210cb3af6f2106a4346b56e4e76b
Sihle-eng <smchobokazi@byupathway.edu>
2026-01-09 11:34:29 +0200

    complete

--- src/js/product.js
+++ src/js/product.js
@@ -4,8 +4,21 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get existing cart or start with empty array
+  let cart = JSON.parse(localStorage.getItem("so-cart"));
+
+  // If cart is not an array (e.g. null or object), reset it
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // Add the new product
+  cart.push(product);
+
+  // Save back to localStorage
+  localStorage.setItem("so-cart", JSON.stringify(cart));
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
