1e84b061caf19bc0c412125db037a9e580f34eaa
Philip Adelabu <adebisiphilip@gmail.com>
2026-01-11 20:10:01 +0100

    fix the cart bug

--- src/js/product.js
+++ src/js/product.js
@@ -4,13 +4,19 @@ import ProductData from "./ProductData.mjs";
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-
+  // console.log(product);
   let cart = getLocalStorage("so-cart") || []; // get existing cart or initialize empty array
-
+  
+  if(! Array.isArray(cart)){  // check to see if the cart is an array format
+    cart = new Array();
+  }
+   
   cart.push(product); // add product to cart array
   setLocalStorage("so-cart", cart); // save updated cart to local storage
   alert("Product added to cart!");
 }
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -21,3 +27,4 @@ async function addToCartHandler(e) {
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+ 
