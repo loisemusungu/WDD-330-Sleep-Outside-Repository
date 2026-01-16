10bb2cf8dfd944af054e875a49bc6b205d672481
loisemusungu <mwongelimusungu@gmail.com>
2026-01-16 09:38:40 +0300

    I added a function for a productCardTemplate(product)

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,3 +1,15 @@
+// function to generate HTML for a single product card
+function productCardTemplate(product) {
+    return `
+      <section class="product-card">
+        <h3>${product.name}</h3>
+        <img src="${product.image}" alt="${product.name}">
+        <p>${product.description}</p>
+        <p class="price">Ksh ${product.price}</p>
+      </section>
+    `;
+  }
+
 export default class ProductList {
     constructor(category, dataSource, listElement) {
     // I passed in this information to make the class as reusable as possible.
