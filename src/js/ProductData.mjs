53c59417169189d85b4bcc82b2a606f359c8a867
loisemusungu <mwongelimusungu@gmail.com>
2026-01-23 21:23:19 +0300

    Added get data and async/await

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,5 @@
+const baseURL = import.meta.env.VITE_SERVER_URL
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -11,11 +13,18 @@ export default class ProductData {
     this.category = category;
     this.path = `../json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  // Fetch products for a specific category
+  async getData(category) {
+    try {
+      const response = await fetch(`${baseURL}products/search/${category}`);
+      const data = await convertToJson(response);
+      return data.Result;
+    } catch (error) {
+      console.error("Error fetching product data:", error);
+      return [];
+    }
   }
+  
   async findProductById(id) {
     const products = await this.getData();
     return products.find((item) => item.Id === id);
