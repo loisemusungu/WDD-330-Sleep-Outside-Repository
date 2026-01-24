18e150fba1f7fd27d34ee1e9846fcd0c63848658
loisemusungu <mwongelimusungu@gmail.com>
2026-01-24 11:47:35 +0300

    Adding product title

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -3,7 +3,7 @@ function productCardTemplate(product) {
     return `
       <li class="product-card">
         <a href="product_pages/?product=${encodeURIComponent(product.NameWithoutBrand)}">
-          <img src="${product.Image}" alt="Image of ${product.NameWithoutBrand}">
+        <img src="${product.PrimaryMedium?.Url}" alt="Image of ${product.NameWithoutBrand}">
           <h2 class="card__brand">${product.Brand.Name}</h2>
           <h3 class="card__name">${product.NameWithoutBrand}</h3>
           <p class="product-card__price">Ksh ${product.FinalPrice}</p>
