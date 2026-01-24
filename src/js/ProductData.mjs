1f6d76c47c5400f5beda4a1bf4a699e11b70e778
budujoseph <budu.joseph95@gmail.com>
2026-01-24 19:37:37 +0000

    wk3 individual activity

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,3 +1,5 @@
+const baseUrl = import.meta.env.VITE_SERVER_URL;
+
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -8,16 +10,20 @@ function convertToJson(res) {
 
 export default class ProductData {
   constructor(category) {
-    this.category = category;
-    this.path = `/json/${this.category}.json`;
+    //this.category = category;
+    //this.path = `../public/json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  async getData(category) {
+    const response = await fetch(`${baseUrl}/products/search/${category}`);
+    const data = await convertToJson(response);
+    return data.Result;
   }
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    const response = await fetch(`${baseUrl}/product/${id}`);
+    const data = await convertToJson(response);
+    console.log(data.Result);
+
+    return data.Result;
   }
+
 }
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         main: "src/index.html",
         cart: "src/cart/index.html",
         checkout: "src/checkout/index.html",
+        product_listing: "src/product_listing/index.html",
       },
     },
   },
